cb7a7ba4bb27aea8895025fb6288c38df64861de
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2024-02-29 19:49:21 -0500

    Initial files set up

---
+++ atm/src/main/java/dev/atm/AtmApplication.java
@@ -0,0 +1,12 @@
+package dev.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
